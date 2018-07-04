93320a2b260d94b344773ee883f92208a3ca661c
Manognarayasam <manognarayasam1998@gmail.com>
2018-07-04 23:46:59 +0530

    Intial Commit

---
+++ Controllers/HomrePageController.cs
@@ -0,0 +1,39 @@
+﻿using School.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace School.Controllers
+{
+    public class HomrePageController : Controller
+    {
+        SchoolEntities SE = new SchoolEntities();
+        // GET: HomrePage
+        public ActionResult Home()
+        {
+            return View();
+        }
+        //public ActionResult basics(int? id)
+        //{
+        //    //if (Session["UserID"] == null)
+        //    //    return RedirectToAction("Login", "common");
+        //    //if (id == null)
+        //    //    return View();
+        //    var data1 = SE.Basics
+        //        .Where(x => x.id == id)
+        //        .Select(x => new Basic
+        //        {
+        //            id = x.id,
+        //            name = x.name,
+        //            emailid = x.emailid,
+        //            phonenumber = x.phonenumber,
+        //            message = x.message
+                   
+        //        }).FirstOrDefault();
+
+        //    return View(data1);
+        //}
+    }
+}
---
+++ Models/Home.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School.Models
+{
+    public class Home
+    {
+    }
+    ////public class basics
+    ////{
+       
+    ////    public int? id { get; set; }
+    ////    public string name { get; set; }
+    ////    public string email_id { get; set; }
+    ////    public int phonenumber { get; set; }
+    ////    public string message { get; set; }
+        
+    ////}
+
+}
